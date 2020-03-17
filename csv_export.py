d5f46520bead04e84be881a2354112f3c0b945b5
Prismary <prismarymail@gmail.com>
2020-03-17 23:17:31 +0100

    Updated export format

--- csv_export.py
+++ csv_export.py
@@ -1,12 +1,14 @@
 import os
 import sqlite3
 import re
+import time
 
 conn = sqlite3.connect('pysom.db')
 conn.create_function('REGEXP', 2, lambda x, y: 1 if re.search(x,y) else 0)
 cursor = conn.cursor()
 
 csv = open('out.csv', 'x')
+csv.write('DayID;Relative;Absolute;Timestamp;Note\n')
 
 cursor.execute(
 	'''SELECT MAX(DayID) FROM "main.Mind";'''
@@ -16,10 +18,11 @@ db_len = cursor.fetchone()[0]
 value = 0
 for i in range(1, db_len+1):
     cursor.execute(
-        '''SELECT Difference FROM "main.Mind"
+        '''SELECT * FROM "main.Mind"
         WHERE DayID = ?;''', (i,)
     )
-    value += cursor.fetchone()[0]
-    csv.write(str(value)+'\n')
+    row = cursor.fetchone()
+    value += row[1]
+    csv.write('{};{};{};{};{}\n'.format(row[0], row[1], str(value), time.ctime(row[3]), row[2]))
 
 csv.close()
